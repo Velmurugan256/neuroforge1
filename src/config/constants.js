16616e42f69a21d00cb64189c3d1b6c4c99dd298
Navya Kola <navyakola74@gmail.com>
2025-09-08 16:13:49 +0530

    renamed to nuerocore title

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 /* Centralized API configuration */
 export const BASE_URL = "https://a4pifj82cl.execute-api.us-east-1.amazonaws.com/prod"
-export const BUCKET = "neuroforge-dev-v-02"
+export const BUCKET = "neurocore-dev-v-02"
 
 export const ENDAVA_API_URL = "https://yb40fa4yg6.execute-api.us-east-1.amazonaws.com/Prod"
 
